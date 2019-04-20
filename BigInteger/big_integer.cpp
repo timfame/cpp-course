4829cf076371a73f605b8be6325e8ad06e9ce321
timfame <timf4me@gmail.com>
2019-04-20 09:01:46 +0300

    start doing bitwise operations

--- BigInteger/big_integer.cpp
+++ BigInteger/big_integer.cpp
@@ -2,6 +2,8 @@
 // Created by timfame on 17.04.19.
 //
 
+#include <algorithm>
+#include <iostream>
 #include "big_integer.h"
 
 big_integer:: big_integer() {
@@ -10,7 +12,7 @@ big_integer:: big_integer() {
 }
 
 big_integer:: big_integer(int new_val) {
-    data = {static_cast<unsigned int>(abs(new_val))};
+    data = {static_cast<bit>(::abs(new_val))};
     negate = new_val < 0;
 }
 
@@ -28,49 +30,202 @@ big_integer:: big_integer(std::string const &new_val) {
     if (new_val[0] == '-') {
         value = -value;
     }
-    this->data = value.data;
-    this->negate = value.negate;
+    data = value.data;
+    negate = value.negate;
 }
 
 big_integer:: ~big_integer() {
     data.clear();
 }
 
-big_integer& big_integer:: operator = (big_integer const& val) {
-    this->data = val.data;
-    this->negate = val.negate;
+big_integer& big_integer:: operator = (big_integer const& other) {
+    if (*this != other) {
+        big_integer(other).swap(*this);
+    }
+    return *this;
 }
 
-big_integer& big_integer:: operator +=(big_integer const& val) {
-    return this + val;
+big_integer& big_integer:: operator +=(big_integer const& other) {
+    if (negate != other.negate) {
+        return *this -= (-other);
+    }
+    bool carry = 0;
+    for (size_t i = 0; i < (std::max(data.size(), other.data.size())) || carry; i++) {
+        if (i == data.size())
+            data.push_back(0);
+        data[i] = data[i] + (i < other.data.size() ? other.data[i] : 0) + carry;
+        carry = data[i] >= BIT_MAX;
+        if (carry) {
+            data[i] -= BIT_MAX;
+        }
+    }
+    check_invariant();
+    return *this;
 }
 
-big_integer& big_integer:: operator -=(big_integer const& val) {
-    return this - val;
+big_integer& big_integer:: operator -=(big_integer const& other) {
+    if (negate != other.negate) {
+        return *this += (-other);
+    }
+    std::vector val = other.data;
+    if (*this < other) {
+        negate = !negate;
+        val = this->data;
+        this->data = other.data;
+    }
+    bool carry = 0;
+    for (size_t i = 0; i < val.size() || carry; i++) {
+        data[i] -= carry + (i < val.size() ? val[i] : 0);
+        carry = data[i] < 0;
+        if (carry) {
+            data[i] += BIT_MAX;
+        }
+    }
+    while (data.size() > 1 && data.back() == 0) {
+        data.pop_back();
+    }
+    check_invariant();
+    return *this;
 }
 
-big_integer& big_integer:: operator *=(big_integer const& val) {
-    return this * val;
+big_integer& big_integer:: operator *=(big_integer const& other) {
+    negate = negate != other.negate;
+    std:: vector <bit> res(data.size() + other.data.size());
+    for (size_t i = 0; i < data.size(); i++) {
+        bit carry = 0;
+        for (size_t j = 0; j < other.data.size() || carry; j++) {
+            long_bit cur = res[i + j] + (long_bit)data[i] * (long_bit)(j < other.data.size() ? other.data[j] : 0) + carry;
+            res[i + j] = bit(cur % BIT_MAX);
+            carry = bit(cur / BIT_MAX);
+        }
+    }
+    while (res.size() > 1 && res.back() == 0) {
+        res.pop_back();
+    }
+    data = res;
+    check_invariant();
+    return *this;
 }
 
-big_integer& big_integer:: operator /=(big_integer const& val) {
-    return this / val;
+big_integer big_integer:: div(int other) {
+    big_integer res(*this);
+    res.negate = res.negate != (other < 0);
+    bit carry = 0;
+    size_t i = res.data.size();
+    while (i > 0) {
+        i--;
+        long_bit cur = res.data[i] + (long_bit)carry * (long_bit)BIT_MAX;
+        res.data[i] = bit(cur / other);
+        carry = bit(cur % other);
+    }
+    while (res.data.size() > 1 && res.data.back() == 0) {
+        res.data.pop_back();
+    }
+    res.check_invariant();
+    return res;
 }
 
-big_integer& big_integer:: operator %=(big_integer const& val) {
-    return this % val;
+int big_integer:: mod(int other) {
+    big_integer res(*this);
+    res.negate = res.negate != (other < 0);
+    bit carry = 0;
+    size_t i = res.data.size();
+    while (i > 0) {
+        i--;
+        long_bit cur = res.data[i] + (long_bit)carry * (long_bit)BIT_MAX;
+        res.data[i] = bit(cur / other);
+        carry = bit(cur % other);
+    }
+    return carry;
 }
 
-big_integer& big_integer:: operator &=(big_integer const& val) {
-    return this & val;
+big_integer& big_integer:: operator /=(big_integer const& other) {
+    big_integer divisor(other);
+    int normalize_value = divisor.normalize();
+    *this *= normalize_value;
+    divisor *= normalize_value;
+    size_t m = data.size(), n = divisor.data.size();
+
 }
 
-big_integer& big_integer:: operator |=(big_integer const& val) {
-    return this | val;
+big_integer& big_integer:: operator %=(big_integer const& other) {
+    return *this -= *this / other * other;
 }
 
-big_integer& big_integer:: operator ^=(big_integer const& val) {
-    return this ^ val;
+big_integer& big_integer:: operator &=(big_integer const& other) {
+    return bitwise_operation(other, [](bit a, bit b) { return a & b;});
+}
+
+big_integer& big_integer:: operator |=(big_integer const& other) {
+    return bitwise_operation(other, [](bit a, bit b) { return a | b;});
+}
+
+big_integer& big_integer:: operator ^=(big_integer const& other) {
+    return bitwise_operation(other, [](bit a, bit b) { return a ^ b;});
+}
+
+template <typename T>
+big_integer& big_integer:: bitwise_operation(big_integer const& other, T foo) {
+    big_integer val(other);
+    std::cout << *this << " ";
+    make_two_complement();
+    std::cout << *this << " ";
+    val.make_two_complement();
+    for (size_t i = 0; i < std::max(data.size(), val.data.size()); i++) {
+        if (i == data.size()) {
+            data.push_back(0);
+        }
+        data[i] = foo(data[i], (i < val.data.size() ? val.data[i] : 0));
+    }
+   // return_to_sign();
+    return *this;
+}
+
+
+big_integer& big_integer:: operator <<=(int other) {
+    if (other < 0) {
+        return *this >>= -other;
+    }
+    size_t create = other / BIT_SIZE;
+    size_t shift = other % BIT_SIZE;
+    std:: vector <bit> new_data(data.size() + create + 1, 0);
+    size_t i = 0;
+    for (; i < create; i++) {
+        new_data[i] = 0;
+    }
+    for (; i < new_data.size() - 1; i++) {
+        new_data[i] |= data[i - create] << shift;
+        new_data[i + 1] |= data[i - create] >> (BIT_SIZE - shift);
+    }
+    data = new_data;
+    check_invariant();
+    return *this;
+}
+
+big_integer& big_integer:: operator >>=(int other) {
+    if (other < 0) {
+        return *this <<= -other;
+    }
+    if (other > data.size() * BIT_SIZE) {
+        return *this = ZERO;
+    }
+    if (negate) {
+        --*this;
+    }
+    size_t destroy = other / BIT_SIZE;
+    size_t shift = other % BIT_SIZE;
+    std:: vector <bit> new_data(data.size() - destroy, 0);
+    new_data[0] = data[destroy] >> shift;
+    for (size_t i = 1; i < new_data.size(); i++) {
+        new_data[i] |= data[i + destroy] >> shift;
+        new_data[i - 1] |= data[i + destroy] << (BIT_SIZE - shift);
+    }
+    data = new_data;
+    if (negate) {
+        ++*this;
+    }
+    check_invariant();
+    return *this;
 }
 
 big_integer big_integer:: operator +() const {
@@ -84,12 +239,174 @@ big_integer big_integer:: operator -() const {
 }
 
 big_integer big_integer:: operator ~() const {
-    return -(*this) - 1;
+    return -*this - 1;
 }
 
-big_integer big_integer:: operator +(big_integer a, big_integer const& b) {
+big_integer& big_integer:: operator ++() {
+    return *this += 1;
+}
+
+big_integer big_integer:: operator ++(int) {
+    big_integer temp(*this);
+    ++*this;
+    return temp;
+}
+
+big_integer& big_integer:: operator --() {
+    return *this -= 1;
+}
+
+big_integer big_integer:: operator --(int) {
+    big_integer temp(*this);
+    --*this;
+    return temp;
+}
+
+big_integer operator +(big_integer const& a, big_integer const& b) {
+    return big_integer(a) += b;
+}
+
+big_integer operator -(big_integer const& a, big_integer const& b) {
+    return big_integer(a) -= b;
+}
+
+big_integer operator *(big_integer const& a, big_integer const& b) {
+    return big_integer(a) *= b;
+}
+
+big_integer operator /(big_integer const& a, big_integer const& b) {
+    return big_integer(a) /= b;
+}
+
+big_integer operator %(big_integer const& a, big_integer const& b) {
+    return big_integer(a) %= b;
+}
+
+big_integer operator &(big_integer const& a, big_integer const& b) {
+    return big_integer(a) &= b;
+}
+
+big_integer operator |(big_integer const& a, big_integer const& b) {
+    return big_integer(a) |= b;
+}
+
+big_integer operator ^(big_integer const& a, big_integer const& b) {
+    return big_integer(a) ^= b;
+}
+
+big_integer operator <<(big_integer const& a, int b) {
+    return big_integer(a) <<= b;
+}
+
+big_integer operator >>(big_integer const& a, int b) {
+    return big_integer(a) >>= b;
+}
+
+bool operator ==(big_integer const& a, big_integer const& b) {
+    return a.negate == b.negate && a.data == b.data;
+}
+
+bool operator !=(big_integer const& a, big_integer const& b) {
+    return !(a == b);
+}
+
+bool operator >(big_integer const& a, big_integer const& b) {
     if (a.negate != b.negate) {
-        return a - (-b);
+        return !a.negate;
     }
-
+    if (a.data.size() != b.data.size()) {
+        return a.data.size() > b.data.size();
+    }
+    size_t i = a.data.size();
+    while (i > 0) {
+        i--;
+        if (a.negate ? a.data[i] < b.data[i] : a.data[i] > b.data[i]) {
+            return true;
+        }
+    }
+    return false;
 }
+
+bool operator <(big_integer const& a, big_integer const& b) {
+    return (b > a);
+}
+
+bool operator >=(big_integer const& a, big_integer const& b) {
+    return !(a < b);
+}
+
+bool operator <=(big_integer const& a, big_integer const& b) {
+    return !(a > b);
+}
+
+void big_integer:: swap(big_integer& b) {
+    std::swap(data, b.data);
+    std::swap(negate, b.negate);
+}
+
+void big_integer:: check_invariant() {
+    while (data.size() > 1 && data.back() == 0) {
+        data.pop_back();
+    }
+    if (data.size() == 1 && data[0] == 0) {
+        negate = false;
+    }
+}
+
+int big_integer:: normalize() {
+    return BIT_MAX / (long_bit)(data.back() + 1);
+}
+
+void big_integer:: make_two_complement() {
+    if (!negate) {
+        return;
+    }
+    for (size_t i = 0; i < data.size(); i++) {
+        data[i] = ~data[i];
+    }
+    ++*this;
+}
+
+void big_integer:: return_to_sign() {
+    if (!negate) {
+        return;
+    }
+    --*this;
+    for (size_t i = 0; i < data.size(); i++) {
+        data[i] = ~data[i];
+    }
+}
+
+big_integer big_integer:: abs() const{
+    big_integer temp(*this);
+    temp.negate = false;
+    return temp;
+}
+
+big_integer abs(big_integer const& b) {
+    return b.abs();
+}
+
+std::string big_integer:: to_string() const{
+    std::string res;
+    big_integer cur(*this);
+    do {
+        res += char(cur.mod(10) + 48);
+        cur = cur.div(10);
+    } while (::abs(cur) > 0);
+    if (negate) {
+        res += "-";
+    }
+    std::reverse(res.begin(), res.end());
+    return res;
+}
+
+std::string to_string(big_integer const& b) {
+    return b.to_string();
+}
+
+std::ostream& operator <<(std::ostream& out, big_integer const& a) {
+    return out << to_string(a);
+}
+
+const big_integer big_integer:: ZERO = 0;
--- BigInteger/big_integer.h
+++ BigInteger/big_integer.h
@@ -10,10 +10,13 @@
 #include <cstdlib>
 
 typedef unsigned int bit;
+typedef unsigned long long long_bit;
 
-const int BIT_SIZE = 32;
+const bit BIT_SIZE = 32;
+const long_bit BIT_MAX = 1ll << 32;
+const bit LONG_BIT_SIZE = 64;
 
-class big_integer {
+struct big_integer {
 
     big_integer();
 
@@ -25,28 +28,76 @@ class big_integer {
 
     explicit big_integer(std::string const& new_val);
 
-    big_integer & operator =(big_integer const& val);
+    big_integer& operator =(big_integer const& other);
 
-    big_integer & operator +=(big_integer const& val);
-    big_integer & operator -=(big_integer const& val);
-    big_integer & operator *=(big_integer const& val);
-    big_integer & operator /=(big_integer const& val);
-    big_integer & operator %=(big_integer const& val);
+    big_integer& operator +=(big_integer const& other);
+    big_integer& operator -=(big_integer const& other);
+    big_integer& operator *=(big_integer const& other);
+    big_integer& operator /=(big_integer const& other);
+    big_integer& operator %=(big_integer const& other);
 
-    big_integer & operator &=(big_integer const& val);
-    big_integer & operator |=(big_integer const& val);
-    big_integer & operator ^=(big_integer const& val);
+    big_integer& operator &=(big_integer const& other);
+    big_integer& operator |=(big_integer const& other);
+    big_integer& operator ^=(big_integer const& other);
+    big_integer& operator <<=(int other);
+    big_integer& operator >>=(int other);
 
     big_integer operator +() const;
     big_integer operator -() const;
     big_integer operator ~() const;
 
-    big_integer operator +(big_integer a, big_integer const& b);
+    big_integer& operator ++();
+    big_integer operator ++(int);
+    big_integer& operator --();
+    big_integer operator --(int);
+
+    friend bool operator ==(big_integer const& a, big_integer const& b);
+    friend bool operator >(big_integer const& a, big_integer const& b);
+
+    big_integer div(int other);
+    int mod(int other);
+    std::string to_string() const;
+    big_integer abs() const;
 
 private:
     std::vector <bit> data;
     bool negate;
+
+    template <typename T>
+    big_integer& bitwise_operation(big_integer const& other, T foo);
+
+    void swap(big_integer& b);
+    void check_invariant();
+    int normalize();
+    void make_two_complement();
+    void return_to_sign();
+
+    static const big_integer ZERO;
 };
 
+big_integer operator +(big_integer const& a, big_integer const& b);
+big_integer operator -(big_integer const& a, big_integer const& b);
+big_integer operator *(big_integer const& a, big_integer const& b);
+big_integer operator /(big_integer const& a, big_integer const& b);
+big_integer operator %(big_integer const& a, big_integer const& b);
+
+big_integer operator &(big_integer const& a, big_integer const& b);
+big_integer operator |(big_integer const& a, big_integer const& b);
+big_integer operator ^(big_integer const& a, big_integer const& b);
+
+big_integer operator <<(big_integer const& a, int b);
+big_integer operator >>(big_integer const& a, int b);
+
+bool operator ==(big_integer const& a, big_integer const& b);
+bool operator !=(big_integer const& a, big_integer const& b);
+bool operator >(big_integer const& a, big_integer const& b);
+bool operator <(big_integer const& a, big_integer const& b);
+bool operator >=(big_integer const& a, big_integer const& b);
+bool operator <=(big_integer const& a, big_integer const& b);
+
+big_integer abs(big_integer const& b);
+
+std::string to_string(big_integer const& b);
+std::ostream& operator <<(std::ostream& out, big_integer const& a);
 
 #endif //BIGINTEGER_BIG_INTEGER_H
